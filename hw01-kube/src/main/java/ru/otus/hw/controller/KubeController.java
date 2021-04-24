56c00437025c34881af883841d4996e1173d3c23
xamejleoh <xamejleoh@list.ru>
2021-04-25 01:03:32 +0300

    Initial project

--- hw01-kube/src/main/java/ru/otus/hw/controller/KubeController.java
+++ hw01-kube/src/main/java/ru/otus/hw/controller/KubeController.java
@@ -15,28 +15,7 @@ public class KubeController {
 
     @GetMapping(name = "/", produces = "application/json")
     public String getVersion() {
-        return String.format("Hello, nice to meet you. Image from dockerHub. applicationId=%s", this.applicationContext.getId());
+        return String.format("Hello, nice to meet you. Image from dockerHub. applicationId=%s", this.applicationContext.getApplicationName());
     }
-
-/*
-    $ wget -qO- http://172.17.0.3:80/actuator/health
-    wget: can't connect to remote host (172.17.0.3): Connection refused
-    $ wget -qO- http://172.17.0.3:80/hw/
-    wget: can't connect to remote host (172.17.0.3): Connection refused
-    $ wget -qO- http://172.17.0.3:80/hw/hello
-
-    wget -qO- http://172.17.0.4:80/actuator/health
-    wget -qO- http://172.17.0.4:8000/actuator/health
-    wget -qO- http://172.17.0.4:8000/hw/
-
-    wget -qO- http://172.17.0.6:8000/hw/
-    wget -qO- http://10.105.14.23:9000/actuator/health
-    wget -qO- http://10.105.14.23:9000/hw/
-
-    wget -qO- http://10.103.136.13:9000/hw/
-
-// 21:24 - манифесты
-    //30:28
-*/
 
 }
