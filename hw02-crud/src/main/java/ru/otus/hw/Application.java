06546356ccee12f3955c2912720b342e4ce8f7bb
xamejleoh <xamejleoh@list.ru>
2021-04-30 00:23:24 +0300

    Work on hw2

--- hw02-crud/src/main/java/ru/otus/hw/Application.java
+++ hw02-crud/src/main/java/ru/otus/hw/Application.java
@@ -2,9 +2,11 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("ru.otus.hw")
 public class Application {
     public static void main(String[] args) {
