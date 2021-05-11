653760a2d1e195946486a881e9088136e1290856
xamejleoh <xamejleoh@list.ru>
2021-05-12 00:12:05 +0300

    Working on hw03

--- hw03-prometheus/src/main/java/ru/otus/hw/controller/CrudController.java
+++ hw03-prometheus/src/main/java/ru/otus/hw/controller/CrudController.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.controller;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
